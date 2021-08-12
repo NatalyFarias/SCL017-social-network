edf9d7e04e36f1eff3480261fba98a054b7af3f9
NatalyFarias <nataly.fariasg@gmail.com>
2021-08-12 16:01:30 -0400

    Updates

---
+++ main.js
@@ -0,0 +1,20 @@
+// Este es el punto de entrada de tu aplicacion
+import { changeRoute, showTemplate } from './lib/router.js';
+import { firebaseConfig } from './lib/config/configFirebase.js';
+
+const init = () => {
+  window.onload = () => {
+    showTemplate(window.location.pathname);
+  };
+  window.onpopstate = () => {
+    if (!window.location.hash.includes('#')) {
+      showTemplate(window.location.pathname);
+    }
+  };
+  window.addEventListener('hashchange', () => {
+    changeRoute(window.location.hash);
+  });
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+};
+init();
